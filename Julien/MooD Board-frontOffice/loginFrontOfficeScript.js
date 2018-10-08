0b04e483a52808ab2b2a41a030850e278b36714c
Mirinkel <chubacca@wanadoo.fr>
2018-10-08 15:50:08 +0200

    updates
    Co-Authored-By: Khaoula2018 <khaoula2018@users.noreply.github.com>
    Co-Authored-By: Kenzou2018 <kenzou2018@users.noreply.github.com>

--- Julien/MooD Board-frontOffice/loginFrontOfficeScript.js
+++ Julien/MooD Board-frontOffice/loginFrontOfficeScript.js
@@ -1,35 +1,41 @@
-var baseUrlApi = 'http://localhost:8080';
-//var baseUrlApi = 'http://localhost/phpmyadmin/db_structure.php?server=1&db=base+de+test&token=ba6c7cd5d0ab045d2960e2b43a938997';
-
-// Create user
-var submitElement = document.querySelector(
-  'input[type=submit]'
-);
-
-submitElement.addEventListener('click',function(event){
-  alert('Monsieur ? je suis le boutton je sais plus');
-    event.preventDefault();
-    var value = document.querySelector(
-      'form.create-user>input[type=text]'
-    ).value;
-    // On va envoyer cette valeur au server
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.open('POST', baseUrlApi+'/post/');
-    httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    httpRequest.send('name='+encodeURIComponent(value));
-    httpRequest.onreadystatechange = function() {
-      if (httpRequest.readyState === XMLHttpRequest.DONE) {
-        if (httpRequest.status === 200) {
-          var response = JSON.parse(httpRequest.responseText);
-          afficherUser(response);
-        } else {
-          alert('Un problème est survenu avec la requête.');
-        }
-      }
+var httpRequest = new XMLHttpRequest();    
+httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState == XMLHttpRequest.DONE) {
+        if (httpRequest.status == 200) {
+        var users = JSON.parse(httpRequest.responseText); 
+        display(users);
+        }else {
+            alert('KO');
+        }};
     }
-  }
-);
 
-function redir(element){
-    document.location.href="LoginAdmin.html";
+
+function getLogin(event) {
+    alert('Monsieur ? je suis le boutton je sais plus');
+    console.log("Youhou");
+    event.preventDefault();
+
+    var login = document.querySelector('div.container-fluid>div.row>input[type=text]').value;
+    console.log(login);
+
+    var password = document.querySelector('div.container-fluid>div.row>input[type=password]').value;
+    password.type ="text";
+    console.log(password); 
+    //document.getElementById("password").innerHTML = password;  
+
+    if((login !== "user" ) || (password !== "mots de pass" )) {
+        alert("Login ou Mot de pass n'est pas correct!");
+        document.location.href='loginFrontOffice.html';
+    } else {
+        document.location.href='MoodChoice.html';
+    }
 }
+
+
+
+    function redir(element){
+        document.location.href="LoginAdmin.html";
+    }
+    /*var submitLogin = document.querySelector('input[type=submit]');
+    console.log(submitLogin);
+    submitLogin.addEventListener('click', getLogin);*/
